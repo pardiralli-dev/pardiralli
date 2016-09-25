967c326821c14218a7b2cf80ecd4006f47a0dea1
Kaspar Papli <kaspar.papli@gmail.com>
2016-09-25 22:31:48 +0300

    Hello, Spring

---
+++ src/main/java/ee/pardiralli/HelloSpringController.java
@@ -0,0 +1,15 @@
+package ee.pardiralli;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloSpringController {
+    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String index() {
+        return "Hello, Spring";
+    }
+}

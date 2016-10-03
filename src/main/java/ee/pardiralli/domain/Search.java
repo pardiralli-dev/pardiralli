f5a316f962020e435d8320bda97c2449396d7f27
PriitPaluoja <priitpaluoja@gmail.com>
2016-10-03 12:19:48 +0300

    * Refactored search model #46

--- src/main/java/ee/pardiralli/domain/Search.java
+++ src/main/java/ee/pardiralli/domain/Search.java
@@ -1,66 +1,19 @@
 package ee.pardiralli.domain;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Search {
 
     private String itemId;
+
     private String buyersEmail;
+
     private String ownersFirstName;
+
     private String ownersLastName;
+
     private String ownersPhoneNr;
-
-    public Search() {
-        this.itemId = "";
-        this.buyersEmail = "";
-        this.ownersFirstName = "";
-        this.ownersLastName = "";
-        this.ownersPhoneNr = "";
-    }
-
-    public Search(String itemId, String buyersEmail, String ownersFirstName, String ownersLastName, String ownersPhoneNr) {
-        this.itemId = itemId;
-        this.buyersEmail = buyersEmail;
-        this.ownersFirstName = ownersFirstName;
-        this.ownersLastName = ownersLastName;
-        this.ownersPhoneNr = ownersPhoneNr;
-    }
-
-    public String getItemId() {
-        return itemId;
-    }
-
-    public String getBuyersEmail() {
-        return buyersEmail;
-    }
-
-    public String getOwnersFirstName() {
-        return ownersFirstName;
-    }
-
-    public String getOwnersLastName() {
-        return ownersLastName;
-    }
-
-    public String getOwnersPhoneNr() {
-        return ownersPhoneNr;
-    }
-
-    public void setItemId(String itemId) {
-        this.itemId = itemId;
-    }
-
-    public void setBuyersEmail(String buyersEmail) {
-        this.buyersEmail = buyersEmail;
-    }
-
-    public void setOwnersFirstName(String ownersFirstName) {
-        this.ownersFirstName = ownersFirstName;
-    }
-
-    public void setOwnersLastName(String ownersLastName) {
-        this.ownersLastName = ownersLastName;
-    }
-
-    public void setOwnersPhoneNr(String ownersPhoneNr) {
-        this.ownersPhoneNr = ownersPhoneNr;
-    }
 }

11abaf6ce0b31adbd2b7268979d2fb8bdd00e427
PriitPaluoja <priitpaluoja@gmail.com>
2016-10-13 17:48:38 +0300

    * Implemented admin search, closes  #57

--- src/main/java/ee/pardiralli/web/SearchController.java
+++ src/main/java/ee/pardiralli/web/SearchController.java
@@ -4,21 +4,19 @@ import ee.pardiralli.db.DuckRepository;
 import ee.pardiralli.domain.Duck;
 import ee.pardiralli.domain.Search;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Controller
 public class SearchController {
-    // TODO: 26.09.16
     private final DuckRepository duckRepository;
-
 
     @Autowired
     public SearchController(DuckRepository duckRepository) {
@@ -36,11 +34,17 @@ public class SearchController {
 
     @PostMapping("/search")
     public String searchSubmit(@ModelAttribute Search userQuery, Model model) {
-        List<Duck> result = new ArrayList<>();
+        List<Duck> result;
 
         if (userQuery.hasOnlyId() && duckRepository.exists(userQuery.getItemId())) {
             result = Collections.singletonList(duckRepository.findById(userQuery.getItemId()));
-        }
+        } else result = duckRepository.findDuck(
+                userQuery.getOwnersFirstName(),
+                userQuery.getOwnersLastName(),
+                userQuery.getBuyersEmail(),
+                userQuery.getOwnersPhoneNr(),
+                new PageRequest(0, 30)
+        );
 
         model.addAttribute("search", new Search());
         model.addAttribute("result", result);

ef3d949b7412f8e294ea5774bb2e2b14497fd140
MariLiis <mariliisvelner@gmail.com>
2016-10-20 18:12:35 +0300

    Statistics controller now supports donation chart.  #15

--- src/main/java/ee/pardiralli/web/StatisticsController.java
+++ src/main/java/ee/pardiralli/web/StatisticsController.java
@@ -1,15 +1,23 @@
 package ee.pardiralli.web;
 
+import ee.pardiralli.db.RaceRepository;
 import ee.pardiralli.domain.DonationChart;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class StatisticsController {
+    private final RaceRepository raceRepository;
+
+    @Autowired
+    public StatisticsController(RaceRepository raceRepository) {
+        this.raceRepository = raceRepository;
+    }
 
     @GetMapping("/statistics")
     public String statistics(Model model) {
@@ -17,10 +25,18 @@ public class StatisticsController {
     }
 
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping("/statistics")
     public
     @ResponseBody
-    DonationChart sayHello() {
-        return new DonationChart();
+    DonationChart setSoldItemsAndDonations(@RequestParam(value = "start", defaultValue = "missing") String start,
+                                           @RequestParam(value = "end", defaultValue = "missing") String end) {
+        System.out.println(start);
+        System.out.println(end);
+        //TODO: assing default values
+        //TODO: sanitize input???
+        //TODO: set default values (last >=7 days)
+        //TODO: admin can only pick legal dates (sanitize input)
+        //TODO: handle wrong input somehow
+        return new DonationChart(start, end, raceRepository);
     }
 }

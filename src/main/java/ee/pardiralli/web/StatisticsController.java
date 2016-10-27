b71b9d167c3a48ab8c29a3df8bb8e4402592fa4a
MariLiis <mariliisvelner@gmail.com>
2016-10-27 18:29:00 +0300

    Gets default data via duckRepository for count of bought ducks given a date and sends it to front-end #15

--- src/main/java/ee/pardiralli/web/StatisticsController.java
+++ src/main/java/ee/pardiralli/web/StatisticsController.java
@@ -1,6 +1,6 @@
 package ee.pardiralli.web;
 
-import ee.pardiralli.db.RaceRepository;
+import ee.pardiralli.db.DuckRepository;
 import ee.pardiralli.domain.DonationChart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,14 +10,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 @Controller
 public class StatisticsController {
-    private final RaceRepository raceRepository;
+    private final DuckRepository duckRepository;
 
     @Autowired
-    public StatisticsController(RaceRepository raceRepository) {
-        this.raceRepository = raceRepository;
+    public StatisticsController(DuckRepository duckRepository) {
+        this.duckRepository = duckRepository;
     }
+
+
 
     @GetMapping("/statistics")
     public String statistics(Model model) {
@@ -28,15 +35,47 @@ public class StatisticsController {
     @PostMapping("/statistics")
     public
     @ResponseBody
-    DonationChart setSoldItemsAndDonations(@RequestParam(value = "start", defaultValue = "missing") String start,
-                                           @RequestParam(value = "end", defaultValue = "missing") String end) {
-        System.out.println(start);
-        System.out.println(end);
-        //TODO: assing default values
-        //TODO: sanitize input???
+    DonationChart setSoldItemsAndDonations(@RequestParam(value = "start", defaultValue = "24.10.2016") String start,
+                                           @RequestParam(value = "end", defaultValue = "31.10.2016") String end) {
+
+
+        //TODO: assign default values
         //TODO: set default values (last >=7 days)
         //TODO: admin can only pick legal dates (sanitize input)
         //TODO: handle wrong input somehow
-        return new DonationChart(start, end, raceRepository);
+        DateFormat formatter = new SimpleDateFormat("dd.MM.yy");
+        Date startDate = null;
+        try {
+            startDate = formatter.parse(start);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Date endDate = null;
+        try {
+            endDate = formatter.parse(end);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(startDate);
+        System.out.println(endDate);
+        return new DonationChart(getDefaultData());
+
+    }
+
+    public List<List<Object>> getDefaultData(){
+        Calendar calendar = Calendar.getInstance();
+        List<List<Object>> defaultData = new ArrayList<>();
+        calendar.add(Calendar.DAY_OF_YEAR, -7);
+        for (int i = 0; i < 7; i++) {
+            calendar.add(Calendar.DAY_OF_YEAR, 1);
+            Date nextDay = calendar.getTime();
+            String d = nextDay.toString().substring(8,10);
+            int ducks = duckRepository.countByDateOfPurchase(nextDay);
+//            double donations = duckRepository.donationsByDateOfPurchase(nextDay);
+            defaultData.add(Arrays.asList(d,ducks,i*100));
+        }
+
+        return defaultData;
     }
 }

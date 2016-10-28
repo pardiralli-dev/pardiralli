576775285d99e73979d838e63cc2376faa148fab
MariLiis <mariliisvelner@gmail.com>
2016-10-29 01:06:11 +0300

    Added new query method for getting the finish date of the last race #15

--- src/main/java/ee/pardiralli/db/RaceRepository.java
+++ src/main/java/ee/pardiralli/db/RaceRepository.java
@@ -1,15 +1,25 @@
 package ee.pardiralli.db;
 
 import ee.pardiralli.domain.Race;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-import java.sql.Date;
+import java.util.Date;
 
 public interface RaceRepository extends CrudRepository<Race, Integer> {
 
-    // Add methods to get Races from db
+    /**
+     * Method for getting the beginning date of the latest race.
+     * @return beginning date
+     */
+    @Query("SELECT MAX(r.beginning) FROM Race r")
+    Date findLastBeginningDate();
 
-    Race findById(Integer id);
-
-    Race findByFinish(Date date);
+    /**
+     * Method for getting the finish date of the latest race.
+     * @return finish date
+     */
+    @Query("SELECT MAX(r.finish) FROM Race r")
+    Date findLastFinishDate();
 }

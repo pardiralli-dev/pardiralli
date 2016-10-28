7fb9c0956388afd80a1d4f506aa02720021828d1
priitpaluoja <priitpaluoja@gmail.com>
2016-10-28 16:40:49 +0300

    Added form validation for search page #76

--- src/main/java/ee/pardiralli/domain/Search.java
+++ src/main/java/ee/pardiralli/domain/Search.java
@@ -5,6 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -12,16 +16,22 @@ import java.util.Date;
 @AllArgsConstructor
 public class Search {
 
+    @Min(0)
     private Integer serialNumber;
 
+    @Size(max = 256)
     private String buyersEmail;
 
+    @Size(max = 100)
     private String ownersFirstName;
 
+    @Size(max = 100)
     private String ownersLastName;
 
+    @Size(max = 50)
     private String ownersPhoneNr;
 
+    @NotNull
     @DateTimeFormat(pattern = "dd-MM-yyyy")
     private Date raceBeginningDate;
 

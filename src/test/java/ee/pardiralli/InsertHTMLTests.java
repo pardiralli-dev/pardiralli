3658dfd4e2ad4fe1c29e3a958d2e68fec9b8997d
priitpaluoja <priitpaluoja@gmail.com>
2016-11-07 19:06:30 +0200

    Created HTML id check for insertion page  #92

---
+++ src/test/java/ee/pardiralli/InsertHTMLTests.java
@@ -0,0 +1,36 @@
+package ee.pardiralli;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:test.properties")
+@RunWith(SpringRunner.class)
+public class InsertHTMLTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void formInputIdsArePresent() throws Exception {
+        String page = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+        assertTrue(page.contains("id=\"ownerFirstName\""));
+        assertTrue(page.contains("id=\"ownerLastName\""));
+        assertTrue(page.contains("id=\"email\""));
+        assertTrue(page.contains("id=\"ownerPhoneNumber\""));
+        assertTrue(page.contains("id=\"numberOfDucks\""));
+        assertTrue(page.contains("id=\"priceOfOneDuck\""));
+        assertTrue(page.contains("id=\"insert\""));
+    }
+}

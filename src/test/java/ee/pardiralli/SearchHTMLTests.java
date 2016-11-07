c45e28d4adf987f4a99f620f3ffdf7b1680d76ee
priitpaluoja <priitpaluoja@gmail.com>
2016-11-07 17:24:35 +0200

    Refactored Search page tests -> SearchHTMLTests. It checks now only that necessary ID's are present. #86

--- src/test/java/ee/pardiralli/SearchHTMLTests.java
+++ src/test/java/ee/pardiralli/SearchHTMLTests.java
@@ -1,13 +1,7 @@
 package ee.pardiralli;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,16 +9,13 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import static junit.framework.TestCase.assertTrue;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:test.properties")
 @RunWith(SpringRunner.class)
-public class PardiralliSearchTests {
+public class SearchHTMLTests {
 
     @LocalServerPort
     private int port;
@@ -49,35 +40,5 @@ public class PardiralliSearchTests {
         assertTrue(page.contains("id=\"email_long\""));
         assertTrue(page.contains("id=\"phone_number_long\""));
         assertTrue(page.contains("id=\"search_date_long\""));
-    }
-
-    @Ignore
-    @Test
-    public void checkAjaxQuerySending() {
-        WebDriver driver = new FirefoxDriver();
-        String url = "http://localhost:" + port + "/search";
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        driver.get(url);
-        // Find the text input element by its name
-        WebElement dateSelect = driver.findElement(By.id("search_date_long"));
-
-        dateSelect.sendKeys(formatter.format(new Date()));
-        WebElement search = driver.findElement(By.id("search_long"));
-        search.click();
-
-        Boolean isJqueryUsed = (Boolean) ((JavascriptExecutor) driver).executeScript("return (typeof(jQuery) != 'undefined')");
-        if (isJqueryUsed) {
-            while (true) {
-                // JavaScript test to verify jQuery is active or not
-                Boolean ajaxIsComplete = (Boolean) (((JavascriptExecutor) driver).executeScript("return jQuery.active == 0"));
-                if (ajaxIsComplete) break;
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                }
-            }
-        } else throw new RuntimeException();
-
-        driver.quit();
     }
 }

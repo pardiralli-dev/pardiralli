07f10be22c26fb9f7444679b74ca96b501e66966
priitpaluoja <priitpaluoja@gmail.com>
2016-11-10 20:42:44 +0200

    Added @NotNull properties and made it sortable by beginning date. #109

--- src/main/java/ee/pardiralli/domain/Race.java
+++ src/main/java/ee/pardiralli/domain/Race.java
@@ -9,26 +9,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.sql.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@ToString
-public class Race {
+public class Race implements Comparable<Race> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     private Integer id;
 
     private Date beginning;
 
     private Date finish;
 
+    private String name;
+
+    private String description;
+
+    @NotNull
+    private Boolean isOpen;
+
     public Race(Date beginning, Date finish){
         this.beginning = beginning;
         this.finish = finish;
     }
 
+    @Override
+    public int compareTo(Race o) {
+        return this.beginning.compareTo(o.getBeginning());
+    }
 }

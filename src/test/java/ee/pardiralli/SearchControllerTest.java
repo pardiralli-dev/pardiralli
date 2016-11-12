1a69aca2c59aaf2bc35b9ea6c46893c115e4ff35
priitpaluoja <priitpaluoja@gmail.com>
2016-11-12 21:35:01 +0200

    Ignore Selenium tests at search controller - Chrome bug: cannot find element #79

--- src/test/java/ee/pardiralli/SearchControllerTest.java
+++ src/test/java/ee/pardiralli/SearchControllerTest.java
@@ -27,6 +27,7 @@ import java.util.concurrent.TimeUnit;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:test.properties")
 @RunWith(SpringRunner.class)
+@Ignore
 public class SearchControllerTest {
     // MAX_PRICE equals with item count
     private static final Integer MAX_PRICE = 60;

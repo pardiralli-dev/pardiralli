500805f78f3c41f52dfa82fcd0127dd6b265b69e
Kaspar Papli <kaspar.papli@gmail.com>
2016-11-12 17:07:43 +0200

    refactored bank response controllers to use path vars, #8

--- src/main/java/ee/pardiralli/web/BankResponseController.java
+++ src/main/java/ee/pardiralli/web/BankResponseController.java
@@ -1,68 +1,25 @@
 package ee.pardiralli.web;
 
+import ee.pardiralli.banklink.Bank;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 public class BankResponseController {
 
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/banklink/lhv/success")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/banklink/{bank}/success")
     @ResponseStatus(value = HttpStatus.OK)
-    public void successLhv(@RequestParam Map<String, String> params) {
+    public void successResponse(@RequestParam Map<String, String> params, @PathVariable Bank bank) {
         // TODO: 7.11.16
         System.out.println(params);
     }
 
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/banklink/lhv/fail")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/banklink/{bank}/fail")
     @ResponseStatus(value = HttpStatus.OK)
-    public void failLhv(@RequestParam Map<String, String> params) {
+    public void failResponse(@RequestParam Map<String, String> params, @PathVariable Bank bank) {
         // TODO: 7.11.16
         System.out.println(params);
     }
 
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/banklink/swedbank/success")
-    @ResponseStatus(value = HttpStatus.OK)
-    public void successSwedbank(@RequestParam Map<String, String> params) {
-        // TODO: 7.11.16
-        System.out.println(params);
-    }
-
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/banklink/swedbank/fail")
-    @ResponseStatus(value = HttpStatus.OK)
-    public void failSwedbank(@RequestParam Map<String, String> params) {
-        // TODO: 7.11.16
-        System.out.println(params);
-    }
-
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/banklink/seb/success")
-    @ResponseStatus(value = HttpStatus.OK)
-    public void successSeb(@RequestParam Map<String, String> params) {
-        // TODO: 7.11.16
-        System.out.println(params);
-    }
-
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/banklink/seb/fail")
-    @ResponseStatus(value = HttpStatus.OK)
-    public void failSeb(@RequestParam Map<String, String> params) {
-        // TODO: 7.11.16
-        System.out.println(params);
-    }
-
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/banklink/nordea/success")
-    @ResponseStatus(value = HttpStatus.OK)
-    public void successNordea(@RequestParam Map<String, String> params) {
-        // TODO: 7.11.16
-        System.out.println(params);
-    }
-
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/banklink/nordea/fail")
-    @ResponseStatus(value = HttpStatus.OK)
-    public void failNordea(@RequestParam Map<String, String> params) {
-        // TODO: 7.11.16
-        System.out.println(params);
-    }
 }

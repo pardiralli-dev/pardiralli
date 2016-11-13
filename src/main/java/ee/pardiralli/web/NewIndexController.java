ab9652e0af62c2cb0f1de62b8f554cfbbe1740b1
Kaspar Papli <kaspar.papli@gmail.com>
2016-11-13 17:50:34 +0200

    prototype of dynamic donation form, #112

--- src/main/java/ee/pardiralli/web/NewIndexController.java
+++ src/main/java/ee/pardiralli/web/NewIndexController.java
@@ -1,11 +1,14 @@
 package ee.pardiralli.web;
 
-import ee.pardiralli.dto.DonationDTO;
+import ee.pardiralli.dto.DonationFormDTO;
+import ee.pardiralli.dto.DonationBoxDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -14,12 +17,29 @@ import javax.validation.Valid;
 public class NewIndexController {
 
     @GetMapping("/idx")
-    public String donationForm(Model model, HttpServletRequest req) {
+    public String donationForm(Model model) {
+        model.addAttribute(DonationFormDTO.DONATION_VARIABLE_NAME, new DonationFormDTO());
         return "donation-form";
     }
 
+    @PostMapping(value = "/idx", params = {"addBox"})
+    public String donationFormAddBox(@ModelAttribute(DonationFormDTO.DONATION_VARIABLE_NAME) DonationFormDTO donation) {
+        donation.getBoxes().add(new DonationBoxDTO());
+        return "donation-form";
+    }
+
+    @PostMapping(value = "/idx", params = {"removeBox"})
+    public String donationFormRemoveBox(DonationFormDTO donation, BindingResult result) {
+        // TODO: 13.11.16
+        return "donation-form";
+    }
+
+
     @PostMapping("/idx/confirm")
-    public String donationFormSubmit(Model model, HttpServletRequest req, @Valid @ModelAttribute DonationDTO donation) {
+    public String donationFormSubmit(Model model, HttpServletRequest req,
+                                     @Valid @ModelAttribute DonationFormDTO donation, BindingResult result) {
+
+
         return "donation-form-confirmation";
     }
 

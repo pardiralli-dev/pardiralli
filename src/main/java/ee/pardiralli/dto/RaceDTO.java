ed711608769ae203da57dc4ae3fc11f0124d58b1
MariLiis <mariliisvelner@gmail.com>
2016-11-23 18:30:59 +0200

    Added fields  #79

--- src/main/java/ee/pardiralli/dto/RaceDTO.java
+++ src/main/java/ee/pardiralli/dto/RaceDTO.java
@@ -1,4 +1,37 @@
 package ee.pardiralli.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RaceDTO {
+
+
+    private Integer id;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @NotNull
+    private java.util.Date beginning;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @NotNull
+    private java.util.Date finish;
+
+    @NotNull
+    private String raceName;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private Boolean isOpen;
+
+    @NotNull
+    private Boolean isNew;
 }

7105db53d9e3bbae15644e7ed699ff46a04aea28
priitpaluoja <priitpaluoja@gmail.com>
2016-11-24 16:49:44 +0200

    Service logic, added email integration, closes #93

---
+++ src/main/java/ee/pardiralli/service/InsertionServiceImpl.java
@@ -0,0 +1,101 @@
+package ee.pardiralli.service;
+
+import ee.pardiralli.db.*;
+import ee.pardiralli.domain.*;
+import ee.pardiralli.dto.InsertionDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class InsertionServiceImpl implements InsertionService {
+
+    private final DuckRepository duckRepository;
+    private final RaceRepository raceRepository;
+    private final OwnerRepository ownerRepository;
+    private final BuyerRepository buyerRepository;
+    private final TransactionRepository transactionRepository;
+    private final MailService mailService;
+
+    @Autowired
+    public InsertionServiceImpl(DuckRepository duckRepository,
+                                RaceRepository raceRepository,
+                                OwnerRepository ownerRepository,
+                                BuyerRepository buyerRepository,
+                                TransactionRepository transactionRepository,
+                                MailService mailService) {
+
+        this.duckRepository = duckRepository;
+        this.raceRepository = raceRepository;
+        this.ownerRepository = ownerRepository;
+        this.buyerRepository = buyerRepository;
+        this.transactionRepository = transactionRepository;
+        this.mailService = mailService;
+    }
+
+
+    @Override
+    public Boolean saveInsertion(InsertionDTO insertionDTO) {
+        List<Duck> duckList = new ArrayList<>();
+
+        Race race = raceRepository.findRaceByIsOpen(true);
+
+        DuckBuyer duckBuyer = new DuckBuyer();
+
+        duckBuyer.setEmail(insertionDTO.getBuyerEmail());
+        duckBuyer = buyerRepository.save(duckBuyer);
+
+
+        DuckOwner duckOwner = new DuckOwner();
+        duckOwner.setFirstName(insertionDTO.getOwnerFirstName());
+        duckOwner.setLastName(insertionDTO.getOwnerLastName());
+        duckOwner.setPhoneNumber(insertionDTO.getOwnerPhoneNumber());
+        duckOwner = ownerRepository.save(duckOwner);
+
+        Transaction transaction = new Transaction();
+        transaction.setIsPaid(true);
+        transaction.setTimeOfPayment(new Timestamp(ZonedDateTime.now(ZoneId.of("Europe/Helsinki"))
+                .truncatedTo(ChronoUnit.MINUTES).toInstant().getEpochSecond() * 1000L));
+        transaction = transactionRepository.save(transaction);
+
+
+        for (int i = 0; i < insertionDTO.getNumberOfDucks(); i++) {
+            Duck duck = new Duck();
+            duck.setDateOfPurchase(new java.sql.Date(Date.from(ZonedDateTime.now(ZoneId.of("Europe/Helsinki")).toInstant()).getTime()));
+            duck.setDuckBuyer(duckBuyer);
+            duck.setDuckOwner(duckOwner);
+            duck.setTransaction(transaction);
+            duck.setPriceCents(insertionDTO.getPriceOfOneDuck());
+            duck.setTimeOfPurchase(new Timestamp(ZonedDateTime.now(ZoneId.of("Europe/Helsinki"))
+                    .truncatedTo(ChronoUnit.MINUTES).toInstant().getEpochSecond() * 1000L));
+
+
+            duck.setSerialNumber(duckRepository.addDuckReturnId(
+                    duck.getDateOfPurchase(),
+                    duckOwner.getFirstName(),
+                    duckOwner.getLastName(),
+                    duckOwner.getPhoneNumber(),
+                    duckBuyer.getEmail(),
+                    duckOwner.getPhoneNumber(),
+                    race.getId(),
+                    duck.getTimeOfPurchase(),
+                    duck.getPriceCents(),
+                    duck.getTransaction().getId()));
+
+            duckList.add(duck);
+        }
+        return mailService.sendConfirmationEmail(duckBuyer,duckList);
+    }
+
+    @Override
+    public boolean OpenedRaceExists() {
+        return raceRepository.countOpenedRaces() == 1;
+    }
+}

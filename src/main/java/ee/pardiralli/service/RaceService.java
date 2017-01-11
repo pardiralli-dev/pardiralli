24a4c070827430dcec0a3525783fedc3f3deb321
kspar <kaspar.papli@gmail.com>
2017-01-11 19:36:24 +0200

    Refactored RaceService

--- src/main/java/ee/pardiralli/service/RaceService.java
+++ src/main/java/ee/pardiralli/service/RaceService.java
@@ -1,5 +1,6 @@
 package ee.pardiralli.service;
 
+import ee.pardiralli.domain.Race;
 import ee.pardiralli.dto.RaceDTO;
 
 import java.util.List;
@@ -7,14 +8,14 @@ import java.util.List;
 public interface RaceService {
 
     /**
-     * Update one race in database according to the raceDto ID
+     * Update one race in database according to the raceDTO ID
      */
-    void updateRace(RaceDTO raceDTO);
+    Race updateRace(RaceDTO raceDTO);
 
     /**
      * Save new race into the database - it opens new race
      */
-    void saveNewRace(RaceDTO raceDTO);
+    Race saveNewRace(RaceDTO raceDTO);
 
     boolean hasNoOpenedRaces();
 
--- src/main/java/ee/pardiralli/service/RaceServiceImpl.java
+++ src/main/java/ee/pardiralli/service/RaceServiceImpl.java
@@ -3,6 +3,7 @@ package ee.pardiralli.service;
 import ee.pardiralli.db.RaceRepository;
 import ee.pardiralli.domain.Race;
 import ee.pardiralli.dto.RaceDTO;
+import lombok.extern.log4j.Log4j;
 import org.apache.commons.collections4.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Log4j
 public class RaceServiceImpl implements RaceService {
 
     private final RaceRepository raceRepository;
@@ -22,15 +24,15 @@ public class RaceServiceImpl implements RaceService {
     }
 
     @Override
-    public void updateRace(RaceDTO raceDTO) {
+    public Race updateRace(RaceDTO raceDTO) {
         Race fromDb = raceRepository.findOne(raceDTO.getId());
         fromDb.setIsOpen(raceDTO.getIsOpen());
-        raceRepository.save(fromDb);
+        return raceRepository.save(fromDb);
     }
 
     @Override
-    public void saveNewRace(RaceDTO raceDTO) {
-        raceRepository.save(
+    public Race saveNewRace(RaceDTO raceDTO) {
+        return raceRepository.save(
                 new Race(new java.sql.Date(raceDTO.getBeginning().getTime()),
                         new java.sql.Date(raceDTO.getFinish().getTime()),
                         raceDTO.getRaceName(),
@@ -51,18 +53,16 @@ public class RaceServiceImpl implements RaceService {
     @Override
     public List<RaceDTO> getAllRacesAsDtos() {
         List<RaceDTO> races = IteratorUtils.toList(
-                raceRepository.findAll().iterator())
-                .stream()
-                .map(r ->
-                        new RaceDTO(
+                raceRepository.findAll().iterator()).stream()
+                .map(r -> new RaceDTO(
                                 r.getId(),
                                 r.getBeginning(),
                                 r.getFinish(),
                                 r.getRaceName(),
                                 r.getDescription(),
                                 r.getIsOpen(),
-                                false)
-                ).collect(Collectors.toList());
+                                false))
+                .collect(Collectors.toList());
         Collections.sort(races);
         return races;
     }

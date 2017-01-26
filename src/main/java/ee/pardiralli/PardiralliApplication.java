b3f0a947dccfd31aad30ffc2fdb63de6260c3496
kspar <kaspar.papli@gmail.com>
2017-01-26 17:14:02 +0200

    SpringBootServletInitializer in main for building war

--- src/main/java/ee/pardiralli/PardiralliApplication.java
+++ src/main/java/ee/pardiralli/PardiralliApplication.java
@@ -2,15 +2,24 @@ package ee.pardiralli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class PardiralliApplication {
+public class PardiralliApplication extends SpringBootServletInitializer {
 
     public static final String MAIN_PROFILE = "development";
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PardiralliApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PardiralliApplication.class, args);
     }
 }
+
+

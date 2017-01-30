94b69c94df5fae4bdca222e218af5c44b865112a
kspar <kaspar.papli@gmail.com>
2017-01-30 12:58:02 +0200

    Main donation form input validation

--- src/main/java/ee/pardiralli/dto/DonationBoxDTO.java
+++ src/main/java/ee/pardiralli/dto/DonationBoxDTO.java
@@ -2,25 +2,30 @@ package ee.pardiralli.dto;
 
 import ee.pardiralli.domain.Duck;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 
 @Data
 public class DonationBoxDTO {
 
     @NotBlank
+    @Length(min = 1, max = 100)
     private String ownerFirstName;
 
     @NotBlank
+    @Length(min = 1, max = 100)
     private String ownerLastName;
 
     @NotBlank
+    @Pattern(regexp = "\\+?[0-9 ]+")
+    @Length(min = 2, max = 50)
     private String ownerPhone;
 
     @NotNull
     @Min(1)
+    @Max(10_000)
     private Integer duckQuantity;
 
     @NotNull
--- src/main/java/ee/pardiralli/dto/DonationFormDTO.java
+++ src/main/java/ee/pardiralli/dto/DonationFormDTO.java
@@ -5,6 +5,8 @@ import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.Valid;
+import javax.validation.constraints.AssertTrue;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -14,11 +16,15 @@ public class DonationFormDTO {
     public static final String DONATION_VARIABLE_NAME = "donation";
 
     @NotEmpty
+    @Valid
     private List<DonationBoxDTO> boxes;
 
     @NotBlank
     @Email
     private String buyerEmail;
+
+    @AssertTrue
+    private Boolean accepts;
 
     public DonationFormDTO() {
         this.boxes = new ArrayList<>(Collections.singletonList(new DonationBoxDTO()));

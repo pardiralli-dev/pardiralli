979b2b07f3442cce2f51b56706cb6757f49b328e
MariLiis <mariliisvelner@gmail.com>
2017-01-31 12:35:51 +0200

    Now language independent and uses LocalDate #131 #136

--- src/main/java/ee/pardiralli/util/StatisticsUtil.java
+++ src/main/java/ee/pardiralli/util/StatisticsUtil.java
@@ -1,8 +1,7 @@
 package ee.pardiralli.util;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class StatisticsUtil {
 
@@ -12,21 +11,13 @@ public class StatisticsUtil {
      * @param startDate first date to format
      * @param endDate   second date to format
      * @return a string in the shape of date1 - date2, where both dates are formatted according to the
-     * SimpleDateFormat object
+     * DateTimeFormatter object
      */
-    public static String getNiceDate(Date startDate, Date endDate) {
-        SimpleDateFormat form = new SimpleDateFormat("dd-MM-yy");
-
-        Calendar start = Calendar.getInstance();
-        start.setTime(startDate);
-
-        Calendar end = Calendar.getInstance();
-        end.setTime(endDate);
-
-        String startFormatted = form.format(start.getTime());
-        String endFormatted = form.format(end.getTime());
-
-        return startFormatted + " kuni " + endFormatted;
+    public static String getNiceDate(LocalDate startDate, LocalDate endDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy");
+        String startFormatted = startDate.format(formatter);
+        String endFormatted = endDate.format(formatter);
+        return startFormatted + " - " + endFormatted;
     }
 
 

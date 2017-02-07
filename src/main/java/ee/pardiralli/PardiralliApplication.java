96fde75fc572836f2c28e0df0c2ef642fd9ae0bd
priitpaluoja <priitpaluoja@gmail.com>
2017-02-07 21:00:39 +0200

    Added reference into TODO WP #149

--- src/main/java/ee/pardiralli/PardiralliApplication.java
+++ src/main/java/ee/pardiralli/PardiralliApplication.java
@@ -4,21 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-@EnableGlobalMethodSecurity(securedEnabled = true)
 public class PardiralliApplication extends SpringBootServletInitializer {
-
-    public static final String MAIN_PROFILE = "development";
+    public static void main(String[] args) {
+        SpringApplication.run(PardiralliApplication.class, args);
+    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(PardiralliApplication.class);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(PardiralliApplication.class, args);
     }
 }
 

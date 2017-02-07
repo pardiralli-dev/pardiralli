1162d4a34e10a854f4b81ac1e53da14333944835
priitpaluoja <priitpaluoja@gmail.com>
2017-02-07 13:38:24 +0200

    Added reference into TODO WP #149

--- src/main/java/ee/pardiralli/domain/Admin.java
+++ src/main/java/ee/pardiralli/domain/Admin.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class Admin {
-    //TODO: this entity should be read from WP database
+    //TODO: this entity should be read from WP database: https://codex.wordpress.org/Database_Description
     private String username;
     private String pswdHash;
     private String role;

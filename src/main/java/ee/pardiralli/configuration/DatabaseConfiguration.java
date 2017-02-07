3e645619f0fefde8c70ddd02360508eb0e2b2e92
priitpaluoja <priitpaluoja@gmail.com>
2017-02-07 21:00:16 +0200

    Added reference into TODO WP #149

---
+++ src/main/java/ee/pardiralli/configuration/DatabaseConfiguration.java
@@ -0,0 +1,44 @@
+package ee.pardiralli.configuration;
+
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DatabaseConfiguration {
+
+    //TODO: http://docs.spring.io/spring-boot/docs/current/reference/html/howto-data-access.html
+    //TODO: add insert secondary datasource, create entitymanagers and configure JPA repositories
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("primary.datasource")
+    public DataSource postgreSqlDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+
+    //@Bean
+    //@ConfigurationProperties("secondary.datasource")
+    //public DataSource wordPressDataSource() {
+    //   return new DataSourceProperties().initializeDataSourceBuilder().build();
+    //}
+
+
+ /* @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean primaryEntityManagerFactory(EntityManagerFactoryBuilder builder) {
+        return builder
+                .dataSource(postgreSqlDataSource())
+                .packages(Duck.class, DuckBuyer.class, DuckOwner.class, Race.class, Transaction.class)
+                .persistenceUnit("PostgreSQL")
+                .build();
+    }*/
+
+}

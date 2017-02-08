9b3a1567c85c01c7f70ec207e910b1e567511680
priitpaluoja <priitpaluoja@gmail.com>
2017-02-08 17:22:25 +0200

    Migrating #149

--- src/main/java/ee/pardiralli/service/UserServiceImpl.java
+++ src/main/java/ee/pardiralli/service/UserServiceImpl.java
@@ -1,38 +1,36 @@
 package ee.pardiralli.service;
 
-import ee.pardiralli.domain.Admin;
+import ee.pardiralli.db.UsersRepository;
+import ee.pardiralli.domain.WpUsers;
 import ee.pardiralli.domain.CurrentUser;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @Slf4j
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class UserServiceImpl implements UserDetailsService {
 
-    //TODO: implement
+    private final UsersRepository usersRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        /*Admin user = getAdminEmail(username)
-                .orElseThrow(() -> {
-                    log.warn("Somebody tried to login with invalid name {}", username);
-                    return new UsernameNotFoundException(String.format("User with username=%s was not found", username));
-                });*/
+        System.out.println(usersRepository.findAll());
 
 
-        //TODO: remove hardcoded user
-        Admin user = new Admin("part", "$P$BBZE7jzPjVxY4VnMgsuYuvM9T.EOZD1", "admin");
+        //WpUsers user = usersRepository.findOne(username);
+                //.orElseThrow(() -> {
+                  //  log.warn("Somebody tried to login with invalid name {}", username);
+                    //return new UsernameNotFoundException(String.format("User with username=%s was not found", username));
+                //});
+
+        //TODO: remove hardcoded user and check why using database fails.
+        WpUsers user = new WpUsers(null,"part", "$P$BBZE7jzPjVxY4VnMgsuYuvM9T.EOZD1");
         return new CurrentUser(user);
-    }
-
-
-    private Optional<Admin> getAdminEmail(String username) {
-        //TODO: get user from WP database
-        return null;
     }
 }

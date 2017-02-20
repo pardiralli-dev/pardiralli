a88cb19411abfbe62889d7f76dab787bc20587d5
TuuleS <tuule.sober@gmail.com>
2017-02-20 15:19:34 +0200

    minor alterations; trying to get this to work #191

--- src/main/java/ee/pardiralli/configuration/TextConfiguration.java
+++ src/main/java/ee/pardiralli/configuration/TextConfiguration.java
@@ -1,13 +1,23 @@
 package ee.pardiralli.configuration;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
-public class TextConfiguration {
+public class TextConfiguration extends WebMvcConfigurerAdapter {
+    @Value("${locale.cookie.name}")
+    private String cookieName;
 
     @Bean
     public MessageSource MessageSource(){
@@ -15,7 +25,25 @@ public class TextConfiguration {
         messageSource.setAlwaysUseMessageFormat(true);
         messageSource.setUseCodeAsDefaultMessage(true);
         messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setBasenames("classpath:applicationText");
+        messageSource.setBasenames("classpath:messages");
         return messageSource;
+    }
+
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("et"));
+        localeResolver.setCookieName(cookieName);
+        return localeResolver;
     }
 }

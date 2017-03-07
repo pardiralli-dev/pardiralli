f225422f1a9026f5e2016e27ccc2fc48d794acff
kspar <kaspar.papli@gmail.com>
2017-03-07 23:55:43 +0200

    Allow cross-origin requests from pardiralli.ee & localhost; hardcode counter query URL to urgas

--- src/main/java/ee/pardiralli/web/CounterAjaxController.java
+++ src/main/java/ee/pardiralli/web/CounterAjaxController.java
@@ -5,18 +5,18 @@ import ee.pardiralli.dto.CounterDTO;
 import ee.pardiralli.service.CounterService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class CounterAjaxController {
     private final CounterService counterService;
 
 
     @GetMapping("/counter_ajax")
-    @ResponseBody
+    @CrossOrigin(origins = {"http://localhost:8080", "http://www.pardiralli.ee"})
     public CounterDTO getDuckCount() {
         return counterService.queryCounter();
     }

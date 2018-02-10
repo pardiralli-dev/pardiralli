7126db3973de07919ce063ac48016c35d1e65d8f
PriitPaluoja <priitpaluoja@gmail.com>
2018-02-10 18:34:56 +0200

    Added email schedule system

--- src/main/java/ee/pardiralli/service/MailServiceImpl.java
+++ src/main/java/ee/pardiralli/service/MailServiceImpl.java
@@ -1,68 +1,27 @@
 package ee.pardiralli.service;
 
 
-import ee.pardiralli.configuration.MailConfiguration;
 import ee.pardiralli.db.TransactionRepository;
-import ee.pardiralli.model.Transaction;
 import ee.pardiralli.dto.EmailSentDTO;
 import ee.pardiralli.dto.PurchaseInfoDTO;
 import ee.pardiralli.dto.TextMsgDTO;
+import ee.pardiralli.model.Transaction;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailAuthenticationException;
-import org.springframework.mail.MailSendException;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Slf4j
 public class MailServiceImpl implements MailService {
-    private final SpringTemplateEngine templateEngine;
-    private final MailConfiguration mailConfiguration;
     private final TransactionRepository transactionRepository;
-
-    @Value("${mail.from}")
-    private String from;
+    private final EmailQueueImpl emailQueue;
 
     @Override
-    @Async
     public void sendConfirmationEmail(PurchaseInfoDTO purchaseInfoDTO) {
-        final Context ctx = new Context();
-        ctx.setVariable("dto", purchaseInfoDTO);
-
-        final String htmlContent = templateEngine.process("email", ctx);
-        JavaMailSender sender = mailConfiguration.getJavaMailSender();
-
-        MimeMessage message = sender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-
-        Transaction transaction = transactionRepository.findById(Integer.valueOf(purchaseInfoDTO.getTransactionID()));
-
-        try {
-            helper.setTo(purchaseInfoDTO.getBuyerEmail());
-            helper.setText(htmlContent, true);
-            helper.setFrom(new InternetAddress(from));
-            helper.setSubject("Pardiralli kinnitus");
-            sender.send(message);
-            transaction.setEmailSent(true);
-            transactionRepository.save(transaction);
-            log.info("Confirmation email sent to {}", purchaseInfoDTO.getBuyerEmail());
-        } catch (MessagingException | MailAuthenticationException | MailSendException e) {
-            log.error("Exception occurred while sending the confirmation email: {}", e);
-            transaction.setEmailSent(false);
-            transactionRepository.save(transaction);
-        }
+        log.info("Adding {} to email queue", purchaseInfoDTO);
+        emailQueue.add(purchaseInfoDTO);
     }
 
     @Override

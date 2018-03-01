e471e392c607932a1453715b712a726c9cd484e4
PriitPaluoja <priitpaluoja@gmail.com>
2018-03-01 08:45:02 +0200

    Remove unchecked warning by checking objects

--- src/main/java/ee/pardiralli/util/ControllerUtil.java
+++ src/main/java/ee/pardiralli/util/ControllerUtil.java
@@ -4,19 +4,16 @@ import ee.pardiralli.feedback.Feedback;
 import ee.pardiralli.feedback.FeedbackType;
 import org.springframework.ui.Model;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ControllerUtil {
 
     /**
      * Adds feedback list to model. This can be later used via Thymeleaf to generate Bootstrap alert dialogs.
      * <p>
-     * For example <code>ControllerUtil.setFeedback(FeedbackType.SUCCESS, "Hello world!")</code> will add a list
+     * For example <code>ControllerUtil.addFeedback(FeedbackType.SUCCESS, "Hello world!")</code> will add a list
      * containing one {@link Feedback} object that can be used to generate
      * HTML tag <code><div class="alert alert-success">Hello world!</div></code>
      * <p>
@@ -24,25 +21,19 @@ public class ControllerUtil {
      * @param model    to add feedback
      * @param type     of the feedback
      * @param feedback as message
-     * @return model which is updated
      */
-    public static Model setFeedback(Model model, FeedbackType type, String feedback) {
+    public static void addFeedback(Model model, FeedbackType type, String feedback) {
         if (model.containsAttribute(Feedback.FEEDBACK_VARIABLE_NAME)) {
-            List<Feedback> feedbacks = (List<Feedback>) model.asMap().get(Feedback.FEEDBACK_VARIABLE_NAME);
-            feedbacks.add(new Feedback(feedback, type));
-            model.addAttribute(Feedback.FEEDBACK_VARIABLE_NAME, feedbacks);
+            List<Feedback> feedbackList = ((List<?>) model.asMap().get(Feedback.FEEDBACK_VARIABLE_NAME)).stream()
+                    .map(e -> (Feedback) e)
+                    .collect(Collectors.toList());
+
+            feedbackList.add(new Feedback(feedback, type));
+            model.addAttribute(Feedback.FEEDBACK_VARIABLE_NAME, feedbackList);
         } else {
             List<Feedback> lst = new ArrayList<>();
             lst.add(new Feedback(feedback, type));
             model.addAttribute(Feedback.FEEDBACK_VARIABLE_NAME, lst);
         }
-        return model;
-    }
-
-    public static String currentDatetime() {
-        String dateTime = ZonedDateTime.now(ZoneId.of("Europe/Helsinki"))
-                .truncatedTo(ChronoUnit.MINUTES)
-                .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        return dateTime.substring(0, dateTime.lastIndexOf(":")) + dateTime.substring(dateTime.lastIndexOf(":") + 1, dateTime.length());
     }
 }

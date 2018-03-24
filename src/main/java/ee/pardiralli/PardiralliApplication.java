dcb6a364454a87ca07ea5aa6e637deef610729d1
kspar <kaspar.papli@gmail.com>
2018-03-24 12:37:33 +0200

    refaktor

--- src/main/java/ee/pardiralli/PardiralliApplication.java
+++ src/main/java/ee/pardiralli/PardiralliApplication.java
@@ -2,14 +2,10 @@ package ee.pardiralli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAsync

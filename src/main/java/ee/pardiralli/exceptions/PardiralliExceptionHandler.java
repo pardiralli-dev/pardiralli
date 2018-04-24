1b75b6d8d1bcc6629ffba879c167d64f6c5db122
PriitPaluoja <priitpaluoja@gmail.com>
2018-04-24 18:43:16 +0300

    In the hope of fixing recursive errors

--- src/main/java/ee/pardiralli/exceptions/PardiralliExceptionHandler.java
+++ src/main/java/ee/pardiralli/exceptions/PardiralliExceptionHandler.java
@@ -1,7 +1,7 @@
 package ee.pardiralli.exceptions;
 
 import ee.pardiralli.service.MailService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,20 +9,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Objects;
+
 @Slf4j
 @ControllerAdvice
-@AllArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PardiralliExceptionHandler extends ResponseEntityExceptionHandler {
-
     private final MailService mailService;
+    private Exception lastException;
 
     @ExceptionHandler(value = Exception.class)
     public String handleGenericException(Exception ex, WebRequest request) {
         log.error("Caught a big one: ", ex);
         log.error("Request info: {}", request.getDescription(true));
-        mailService.sendAdminNotification("Uncaught exception: " + ex);
 
-        return "redirect:/?error";
+        if (lastException == null || !Objects.equals(lastException.getMessage(), ex.getMessage())) {
+            mailService.sendAdminNotification("Uncaught exception: " + ex);
+            lastException = ex;
+            return "redirect:/?error";
+        }
+        return "error_r";
     }
 
 }

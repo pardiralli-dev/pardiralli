0f49dbb3b083ef0c1321f3f39bbf8e5fdd619298
karlrobertp <k.r.pildre@gmail.com>
2019-04-28 20:42:38 +0300

    Testing can I add coop bank

---
+++ src/main/java/ee/pardiralli/banklink/CoopRequestModel.java
@@ -0,0 +1,39 @@
+package ee.pardiralli.banklink;
+
+import ee.pardiralli.util.BanklinkUtil;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+
+@Getter
+@Setter
+public class CoopRequestModel extends RequestModel {
+    public static final String senderID = "evlvl";
+
+    private String VK_SERVICE = "1011";
+    private String VK_VERSION = "008";
+    private String VK_SND_ID = senderID;
+    private String VK_STAMP;
+    private String VK_AMOUNT;
+    private String VK_CURR = "EUR";
+    private String VK_ACC = "EE294204278609755209";
+    private String VK_NAME = "EESTI VÄHIHAIGETE LASTE VANEMATE LIIT";
+    private String VK_REF = "";
+    private String VK_MSG;
+    private String VK_RETURN = String.format("https://www.pardiralli.ee/annetus/banklink/%s/success", Bank.coop);
+    private String VK_CANCEL = String.format("https://www.pardiralli.ee/annetus/banklink/%s/fail", Bank.coop);
+    private String VK_DATETIME = BanklinkUtil.currentDateTimeAsString();
+    private String VK_MAC;
+    private String VK_ENCODING = "UTF-8";
+    private String VK_LANG = "EST";
+
+    public CoopRequestModel(String amount, String stamp, String paymentDescription) {
+        this.VK_AMOUNT = amount;
+        this.VK_STAMP = stamp;
+        this.VK_MSG = paymentDescription;
+        this.VK_MAC = BanklinkUtil.getMAC("coop-private.der",
+                Arrays.asList(VK_SERVICE, VK_VERSION, VK_SND_ID, VK_STAMP, VK_AMOUNT, VK_CURR, VK_ACC, VK_NAME,
+                        VK_REF, VK_MSG, VK_RETURN, VK_CANCEL, VK_DATETIME));
+    }
+}

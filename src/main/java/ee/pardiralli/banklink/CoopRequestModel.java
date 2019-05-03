b5ad71c6d792414e9e991db9dd51ef5c309fcb82
karlrobertp <k.r.pildre@gmail.com>
2019-05-03 22:54:27 +0300

    working on coop

--- src/main/java/ee/pardiralli/banklink/CoopRequestModel.java
+++ src/main/java/ee/pardiralli/banklink/CoopRequestModel.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 @Getter
 @Setter
 public class CoopRequestModel extends RequestModel {
-    public static final String senderID = "evlvl";
+    public static final String senderID = "VAHILAPSED";
 
     private String VK_SERVICE = "1011";
     private String VK_VERSION = "008";

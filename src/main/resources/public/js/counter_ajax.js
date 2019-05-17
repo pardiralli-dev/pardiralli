c3f18f24b62f3d871d98d573d8ef2ff31188e0d7
karlrobertp <k.r.pildre@gmail.com>
2019-05-18 00:21:31 +0300

    .

--- src/main/resources/public/js/counter_ajax.js
+++ src/main/resources/public/js/counter_ajax.js
@@ -3,6 +3,7 @@ var counter = 0;//set this to what ever you want the start # to be
 	countUP();//call the function once	
 	function countUP () {
 		counter++;//increment the counter by 1
-		setTimeout ( "countUP()", 1000 );//runs itsself after 1000 miliseconds
+      setTimeout ( "countUP()", 1000 );//runs itsself after 1000 miliseconds	
+      jQuery( ".counter" ).append( "<p>"+ counter +"</p>" );
 		console.log(counter);
 	}

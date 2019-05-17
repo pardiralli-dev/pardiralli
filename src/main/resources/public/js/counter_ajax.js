362ea01160c32ba33cbb8ddd9d06ff3fab9ce3fa
karlrobertp <k.r.pildre@gmail.com>
2019-05-18 00:38:35 +0300

    .

--- src/main/resources/public/js/counter_ajax.js
+++ src/main/resources/public/js/counter_ajax.js
@@ -1,13 +1,11 @@
-// var counter = 0;//set this to what ever you want the start # to be
+var counter = 0;//set this to what ever you want the start # to be
 // var ducks = data.duckCount;
-   
-countUP();//call the function once	
-function countUP () {
-   for(counter = 0; counter < 4705; counter++) {
-      // counter++;//increment the counter by 1
-      setTimeout ( "countUP()", 1 );//runs itsself after 1000 miliseconds	
+	countUP();//call the function once	
+	function countUP () {
+      counter++;//increment the counter by 1
+      if (counter < 4705) {
+         setTimeout ( "countUP()", 1 );//runs itsself after 1000 miliseconds	
+      }
       jQuery( ".counter" ).html( "<p>"+ counter +"</p>" );
-      // console.log(counter);
-   }
-}
-	
+		// console.log(counter);
+	}

57e34081291be6ec0687c4004695f6f049c1e6cc
karlrobertp <k.r.pildre@gmail.com>
2019-05-18 00:02:56 +0300

    lets try again

--- src/main/resources/public/js/counter_ajax.js
+++ src/main/resources/public/js/counter_ajax.js
@@ -1,22 +1,6 @@
-jQuery('.counter').each(function() {
-   var $this = $(this),
-       countTo = $this.attr('data-count');
-   
-   $({ countNum: $this.text()}).animate({
-     countNum: countTo
-   },
- 
-   {
- 
-     duration: 8000,
-     easing:'linear',
-     step: function() {
-       $this.text(Math.floor(this.countNum));
-     },
-     complete: function() {
-       $this.text(this.countNum);
-       //alert('finished');
-     }
- 
-   });  
- });
+jQuery(document).ready(function($) {
+   $('.counter').counterUp({
+       delay: 10,
+       time: 1000
+   });
+});

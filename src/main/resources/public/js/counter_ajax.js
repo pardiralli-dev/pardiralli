86e75c3f118f5890f1222a9c8955ee19e66a9160
karlrobertp <k.r.pildre@gmail.com>
2019-05-18 00:14:28 +0300

    .

--- src/main/resources/public/js/counter_ajax.js
+++ src/main/resources/public/js/counter_ajax.js
@@ -1,6 +1,8 @@
-jQuery(document).ready(function($) {
-   $('.counter').counterUp({
-       delay: 10,
-       time: 1000
-   });
-});
+var counter = 0;//set this to what ever you want the start # to be
+var ducks = data.duckCount;
+	countUP();//call the function once	
+	function countUP () {
+		counter++;//increment the counter by 1
+		setTimeout ( "countUP()", 1000 );//runs itsself after 1000 miliseconds
+		//console.log(counter);uncomment to can see it in action, only with firebug
+	}
